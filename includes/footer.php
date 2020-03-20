5d0ed50539a485c0aae29f202459ad9874ce7d98
Ev3rdown <35498857+Ev3rdown@users.noreply.github.com>
2020-03-20 16:52:23 +0100

    C/c

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer style="background-color: #DEDEDE; padding: 15px; text-align: center; position: absolute; bottom: 0; left: 0; right: 0">
-    <a href="contact.php">Contact</a>
+    <a href="Contact.php">Contact</a>
 </footer>
