064fc7f43335894c74b91340ff4b351029944d70
Ev3rdown <35498857+Ev3rdown@users.noreply.github.com>
2020-03-20 17:33:16 +0100

    thierry et les tests...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 function uneAlert(elmt,produit) {
     switch (produit) {
-        case 'produit1': document.getElementById('result').innerText = oui;
+        case 'produit1': document.getElementById('result').innerText = "oui";
         break;
-        case 'produit2': document.getElementById('result').innerText = non;
+        case 'produit2': document.getElementById('result').innerText = "non";
         break;
     }
 }
