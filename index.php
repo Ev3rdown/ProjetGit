0d3601219e207bf731139418e9a8d401601867dc
Mixm33 <maxime.bertin1@epsi.fr>
2020-03-20 15:10:20 +0100

    Update index.php
    modif du titre
    ajout du header et du footer

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Shop</title>
 </head>
 <body>
-    
+    <header>
+        <a href="index.php">Home</a>
+        <a href="">Shop</a>
+    </header>
+
+    <footer style="position: absolute; bottom:0; padding-bottom: 10px; background-color: #DEDEDE; margin: 0 auto;">
+        <a href="">Contact</a>
+    </footer>
 </body>
 </html>
